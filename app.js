46263a3b445b45b89ee3a00e3e9ca30a282eab0a
zyl <linxi1909992592@163.com>
2022-07-11 20:47:58 +0800

    init -2

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-// app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch: function(options){
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
   },
-  globalData: {
-    userInfo: null
+
+  onShow: function(options){
+
+  },
+
+  onHide: function(){
+
+  },
+
+  onError: function(msg){
+
+  },
+
+  onPageNotFound: function(options){
+    
   }
-})
+});
