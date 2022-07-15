3d42e4c7d0842d9d4afbfd9740687f7f03d08b63
zyl <linxi1909992592@163.com>
2022-07-15 14:44:52 +0800

    promise request

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,6 @@
+//0 引入 用来发送请求的 方法 一定要把路径补全
+import { request } from "../../request/index.js";
+
 Page({
   data: {
     // 轮播图数组
@@ -6,17 +9,24 @@ Page({
 
   // 页面加载就会触发
   onLoad: function(options){
-    //1 发送异步请求获取轮播图数据
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata', //域名需要备案
-      success: (result) => {
-        this.setData({
-          swiperList: result.data.message
+    //1 发送异步请求获取轮播图数据 优化的手段可以通过es6的 promise来解决这个问题
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata', //域名需要备案
+    //   success: (result) => {
+    //     this.setData({
+    //       swiperList: result.data.message
           
-        })
-        console.log(result);
-      }
-    });
+    //     })
+    //     console.log(result);
+       
+    //   }
+    // });
+    request({url:"https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata"})
+    .then(result => {
+      this.setData({
+        swiperList: result.data.message
+      })
+    })
   }
 
 });
