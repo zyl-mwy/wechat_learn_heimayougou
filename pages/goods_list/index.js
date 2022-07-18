f226241ff1b3ddf291f8494044435e2375c4e150
zyl <linxi1909992592@163.com>
2022-07-18 22:12:42 +0800

    pull dowm to refresh the page of goods list

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -14,6 +14,14 @@
     // 2 重新发送请求
     // 3 数据请求回来 要对data中的数组 进行 拼接 而不是全部替换!!!
 
+// 2 下拉刷新页面
+  // 1 触发下拉刷新事件 需要在页面中的json文件中开启一个配置项
+    // 找到触发下拉刷新的事件
+  // 2 重置 数据 数组
+  // 3 重置页码 设置为1
+  // 4 重新发送请求
+  // 5 数据请求回来 需要手动的关闭 等待效果
+
 import { request } from "../../request/index.js";
 import regeneratorRuntime from '../../lib/runtime/runtime';
 
@@ -79,6 +87,9 @@ Page({
       // 拼接了数组
       goodsList:[...this.data.goodsList, ...res.goods]
     })
+
+    // 关闭下拉刷新的窗口 如果没有调用下拉刷新的窗口 直接关闭也不会报错
+    wx.stopPullDownRefresh();
   },
 
   /**
@@ -161,5 +172,17 @@ Page({
       this.QueryParams.pagenum++;
       this.getGoodsList();
     }
+  },
+  // 下拉刷新事件
+  onPullDownRefresh(){
+    console.log("刷新");
+    // 1 重置数组
+    this.setData({
+      goodsList: []
+    })
+    // 2 重置页码
+    this.QueryParams.pagenum = 1;
+    // 3 重新发送请求
+    this.getGoodsList();
   }
 })
