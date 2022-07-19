b1f03a50eaf8b6e65434a723deee3ef2b34b5098
zyl <linxi1909992592@163.com>
2022-07-19 20:57:27 +0800

    get detail of goods

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,18 +1,35 @@
-// pages/goos_detail/index.js
+// 1 发送请求获取数据
+
+
+import {request} from "../../request/index.js"
+import regeneratorRuntime from "../../lib/runtime/runtime"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    // 获取navigationbar url发送的id
+    const {goods_id} = options;
+    console.log(goods_id);
+    this.getGoodsDetail(goods_id);
+  },
 
+  // 获取商品信息详情
+  async getGoodsDetail(goods_id){
+    const goodsObj = await request({url: '/goods/detail', data: {goods_id}});
+    console.log(goodsObj);
+    this.setData({
+      goodsObj
+    })
   },
 
   /**
