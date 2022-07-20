8f650025ea250fcbfcd7f9f6a9b1b67ab94a34af
zyl <linxi1909992592@163.com>
2022-07-20 20:53:18 +0800

    add goods to shop car

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -2,6 +2,13 @@
 // 2 点击轮播图 预览大图
   // 1 给轮播图绑定点击事件
   // 2 调用小程序api previewImage
+// 3 点击加入购物车
+  // 1 先绑定点击事件
+  // 2 获取缓存中的购物车数据 数组格式
+  // 3 先判断 当前的商品是否已经存在于 购物车
+  // 4 已经存在 修改商品数据 执行购物车数量++ 重新把购物车数组 填充回缓存中
+  // 5 不存在于购物车数组中 直接给购物车数组添加一个新元素 新元素 带上 购买数量属性 num 重新把购物车数组 填充回缓存中
+  // 6 弹出提示
 
 import {request} from "../../request/index.js"
 import regeneratorRuntime from "../../lib/runtime/runtime"
@@ -61,6 +68,32 @@ Page({
       current,
       urls
     })
+  },
+
+  // 点击加入购物车
+  handleCartAdd(){
+    console.log("购物车");
+    // 1 获取缓存中的购物车数组
+    let cart = wx.getStorageSync("cart")||[];
+    // 2 判断 商品对象是否存在于购物车数组中
+    let index = cart.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+    if(index === -1){
+      // 不存在 第一次添加
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo);
+    }else{
+      // 已经存在购物车数据 执行 num++
+      cart[index].num++;
+    }
+    // 5 把购物车数据重新添加回缓存中
+    wx.setStorageSync("cart", cart);
+    // 6 弹窗提示
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      // true 防止用户 手抖 疯狂点击按钮
+      mssk: true
+    });
   },
 
   /**
