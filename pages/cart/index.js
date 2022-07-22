5f4334f1e509f211a29d46ec05010d3a81615e1e
zyl <linxi1909992592@163.com>
2022-07-22 14:29:19 +0800

    edit the quantity of goods

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -39,7 +39,20 @@
   // 3 商品对象的选中状态 取反
   // 4 重新填充回data中和缓存中共
   // 5 重新计算全选。总价格 总数量。。。
-
+// 7 全选和反选
+  // 1 全选复选框绑定事件 change
+  // 2 获取 data中的全选变量 allChecked
+  // 3 直接取反 allChecked = !allChecked
+  // 4 遍历购物车数组 让里面 商品 选中状态跟随 allChecked 改变而改变
+  // 5 把购物车数组 和 allChecked 重新设置回data 把购物车重新设置回 缓存中
+// 8 商品数量的编辑
+  // 1 "+" "-" 按钮 绑定同一个点击事件 区分的关键 自定义属性
+    // 1 "+" "+1"
+    // 2 "-" "-1"
+  // 2 传递被点击的商品id goods_id
+  // 3 获取data中的购物车数组 来获取需要被修改的商品对象
+  // 4 直接修改商品对象的数量 num
+  // 5 把cart数组 重新设置回 缓存中 和data中 this.setCart
 
 import { getSetting, chooseAddress, openSetting } from "../../utils/asyncWx.js";
 import regeneratorRuntime from '../../lib/runtime/runtime'
@@ -234,7 +247,7 @@ Page({
   },
   // 设置购物车状态同时 重新计算 底部工具栏的数据 全选 总价格 购买的数量 
   setCart(cart){
-    let allCheck = true;
+    let allChecked = true;
     let totalPrice = 0;
     let totalNum = 0;
     cart.forEach(v => {
@@ -242,15 +255,41 @@ Page({
         totalPrice += v.num * v.goods_price;
         totalNum += v.num;
       }else{
-        allCheck = false;
+        allChecked = false;
       }
     })
     // 判断数组是否为空
-    allCheck = cart.length != 0 ? allCheck : false;
+    allChecked = cart.length != 0 ? allChecked : false;
     this.setData({
       cart,
-      totalPrice, totalNum, allCheck
+      totalPrice, totalNum, allChecked
     });
     wx.setStorageSync('cart', cart);
+  },
+
+  // 商品全选功能
+  handleItemAllCheck(){
+    // 1 获取data中的数据
+    let {cart, allChecked} = this.data;
+    // 2 修改值
+    allChecked = !allChecked;
+    // 3 循环修改cart数组 中的商品选中状态
+    cart.forEach(v=>v.checked=allChecked);
+    // 4 把修改后的值 填充回data或者缓存中
+    this.setCart(cart);
+  },
+
+  handleItemNumEdit(e){
+    // 1 获取事件传递过来的参数
+    const { operation, id } = e.currentTarget.dataset;
+    console.log(operation, id);
+    // 2 获取购物车数组
+    let { cart } = this.data;
+    // 3 找到需要修改的商品的索引
+    const index = cart.findIndex(v => v.goods_id === id);
+    // 4 找到修改数量
+    cart[index].num += operation;
+    // 5 设置回缓存中和data中
+    this.setCart(cart);
   }
 })
