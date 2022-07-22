8cfd9f82aac036b487372b61733a80b873d0f713
zyl <linxi1909992592@163.com>
2022-07-22 19:19:49 +0800

    the analysis of payment permission

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -1,6 +1,12 @@
   // 1 页面加载的时候
     // 1 从缓存中获取购物车数据 渲染到页面中
       // 这些数据 checked=true
+  // 2 微信支付
+    // 1 哪些人 哪些账号 可以实现微信支付
+      // 1 企业账号
+      // 2 企业账号的小程序后台中 必须 给开发者 添加上白名单
+        // 1 一个 appid 可以同时绑定多个开发者
+        // 2 这些开发者就可以公用这个appid 和 它的开发权限
   
   import { getSetting, chooseAddress, openSetting, showModal, showToast } from "../../utils/asyncWx.js";
   import regeneratorRuntime from '../../lib/runtime/runtime'
